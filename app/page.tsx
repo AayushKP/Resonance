fedfb0daca0c1bc2b2fcdacfa43111e40083b8e8
Ayush <ayushprokashyap@gmail.com>
2025-04-24 13:40:22 +0530

    background moved backward

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import { Menu } from "lucide-react";
 export default function Home() {
   return (
     <div className="relative min-h-screen overflow-hidden font-sans">
-      <Background />
+      <div className="fixed inset-0 z-[-10]">
+        <Background />
+      </div>
 
-      <header className="fixed top-6 left-1/2 transform -translate-x-1/2 h-20 w-[90%] md:w-[70%] bg-[#8e8e8e46] rounded-3xl px-6 py-3 flex justify-between items-center z-20 shadow-md">
-        <div className="flex items-baseline space-x-2  font-extrabold text-white text-2xl">
-          <span className="tracking-widest text-3xl font-sanskrit">
+      <header className="fixed top-6 left-1/2 transform -translate-x-1/2 dark:border-neutral-700 shadow backdrop-blur-2xl h-20 w-[90%] md:w-[70%] bg-white/20 dark:bg-neutral-800/20 rounded-3xl px-6 py-3 flex justify-between items-center z-30">
+        <div className="flex items-baseline space-x-2 font-extrabold text-white text-2xl">
+          <span className="tracking-widest text-4xl font-sanskrit">
             Resonance
           </span>
         </div>
@@ -17,7 +19,7 @@ export default function Home() {
         </button>
       </header>
 
-      <main className="relative z-10 flex flex-col items-center justify-center text-center text-white px-4 pt-60">
+      <main className="relative z-40 flex flex-col items-center justify-center text-center text-white px-4 pt-60">
         <h1 className="text-8xl text-red-500/70 font-sanskrit mb-1 tracking-wider">
           Resonance
         </h1>
