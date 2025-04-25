2f5a0c1f49237036c335efd1e768de320b7b7b21
Ayush <ayushprokashyap@gmail.com>
2025-04-25 17:20:27 +0530

    Navbar-wrapped

---
+++ components/Navbar.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
+const Navbar = () => {
+  const pathname = usePathname();
+
+  const routes = [
+    { path: "/", label: "Home" },
+    { path: "/gallery", label: "Gallery" },
+    { path: "/events", label: "Events" },
+  ];
+
+  return (
+    <div className="fixed top-6 left-1/2 transform -translate-x-1/2 shadow backdrop-blur-2xl h-20 w-[90%] md:w-[85%] bg-white/20 rounded-3xl px-12 py-3 flex justify-between items-center z-30">
+      <div className="flex items-center justify-center space-x-3 font-extrabold text-white text-2xl">
+        <div>
+          <Image
+            src="/images/hitk-logo.png"
+            alt="resonance logo"
+            width={50}
+            height={50}
+          />
+        </div>
+        <div>
+          <span className="text-4xl font-sanskrit">
+            <span className="text-metal">Reso</span>
+            nance
+          </span>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center space-x-4 text-xl text-white font-sanskrit tracking-widest">
+        <div className="flex space-x-6">
+          {routes.map(({ path, label }) => (
+            <Link
+              key={path}
+              href={path}
+              className={clsx("cursor-pointer  ", {
+                "text-yellow-400": pathname === path,
+              })}
+            >
+              {label}
+            </Link>
+          ))}
+        </div>
+        <Link
+          href="/achievements"
+          className={clsx(
+            "cursor-pointer bg-gradient-to-r from-yellow-800 via-yellow-500 to-yellow-900 text-center py-1 px-3 rounded-lg",
+            {
+              "text-yellow-400": pathname === "/achievements",
+            }
+          )}
+        >
+          Achievements
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
