3fbc45f874fcfd2d1d4ba76ae1ad191495616245
Ayush <ayushprokashyap@gmail.com>
2025-04-26 16:47:53 +0530

    z-index fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="fixed top-6 left-1/2 transform -translate-x-1/2 shadow backdrop-blur-2xl h-20 w-[90%] md:w-[85%] bg-white/20 rounded-3xl px-12 py-3 flex justify-between items-center z-30">
+    <div className="fixed top-6 left-1/2 transform -translate-x-1/2 shadow backdrop-blur-2xl h-20 w-[90%] md:w-[85%] bg-white/20 rounded-3xl px-12 py-3 flex justify-between items-center z-50">
       <div className="flex items-center justify-center space-x-3 font-extrabold text-white text-2xl">
         <div>
           <Image
