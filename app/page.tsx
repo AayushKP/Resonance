f4477356f42f80c92ff977571f45abccb2e4ac6b
Ayush <ayushprokashyap@gmail.com>
2025-05-08 14:05:04 +0530

    footer-modified

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
 
           <div className="relative z-10 lg:px-20">
             <div className="text-center">
-              <h2 className=" text-5xl md:text-7xl font-cinzel-decorative mb-4 items-center">
+              <h2 className=" text-5xl md:text-6xl font-cinzel-decorative mb-4 items-center">
                 About <span className="text-metal">Resonance </span>
               </h2>
             </div>
@@ -46,7 +46,7 @@ export default function Home() {
                 />
               </div>
               <div>
-                <p className="text-xl md:text-2xl font-tagesschrift text-center lg:text-left text-white leading-relaxed  px-1 md:px-0">
+                <p className="text-xl md:text-2xl font-montserrat text-justify text-white leading-relaxed  px-3 md:px-5">
                   Resonance is not just another college club - it is a
                   collective of excellence, a crucible for talent. This is a
                   place where musical excellence is cultivated, celebrated, and
@@ -62,7 +62,7 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="mt-14 text-5xl md:text-7xl  font-cinzel-decorative w-full mb-10">
+        <section className="mt-14 text-5xl md:text-6xl  font-cinzel-decorative w-full mb-10">
           CO<span className="text-metal">ORDINATORS</span>
           <div className="flex flex-col items-center justify-center gap-10 py-10 px-4">
             <div className="flex flex-wrap justify-center gap-6 relative">
@@ -71,7 +71,7 @@ export default function Home() {
                 .map((coordinator, i) => (
                   <div
                     key={`c1-${i}`}
-                    className="w-24 h-24 md:w-32 md:h-32 lg:w-44 lg:h-44 rounded-full bg-transparent relative overflow-hidden"
+                    className="w-28 h-28 md:w-32 md:h-32 lg:w-44 lg:h-44 rounded-full bg-transparent relative overflow-hidden"
                   >
                     <Image
                       src={coordinator.src}
@@ -90,7 +90,7 @@ export default function Home() {
                 .map((coordinator, i) => (
                   <div
                     key={`c2-${i}`}
-                    className="w-24 h-24 md:w-32 md:h-32 lg:w-44 lg:h-44 rounded-full bg-transparent relative overflow-hidden"
+                    className="w-28 h-28 md:w-32 md:h-32 lg:w-44 lg:h-44 rounded-full bg-transparent relative overflow-hidden"
                   >
                     <Image
                       src={coordinator.src}
@@ -107,52 +107,70 @@ export default function Home() {
         </section>
       </main>
 
-      <footer className="h-60 w-full  backdrop-blur-xl bg-black/20 flex justify-center  p-5">
-        <div className="text-3xl font-cinzel-decorative text-white">Footer</div>
-      </footer>
-      <div className="absolute bottom-0 right-0 p-4 z-50">
-        <div className="flex space-x-6 items-center bg-white/20 backdrop-blur-md rounded-2xl p-3">
+      <footer className="w-full backdrop-blur-xl bg-black/20 p-5 flex flex-col items-center gap-5 font-cinzel-decorative text-white relative">
+        <div className="text-3xl">
+          Contact <span className="text-metal">Us</span>
+        </div>
+
+        <div className="flex flex-col items-center gap-4 text-sm">
+          <div className="flex flex-wrap justify-center gap-4">
+            {coordinators.slice(0, 7).map((coordinator, index) => (
+              <a
+                key={index}
+                href={`mailto:${coordinator.mail}`}
+                target="_blank"
+                className="cursor-pointer hover:text-yellow-500"
+              >
+                {coordinator.name}
+              </a>
+            ))}
+          </div>
+          <div className="flex flex-wrap justify-center gap-4">
+            {coordinators.slice(7, 10).map((coordinator, index) => (
+              <a
+                key={index + 7}
+                href={`mailto:${coordinator.mail}`}
+                target="_blank"
+                className="cursor-pointer hover:text-yellow-500"
+              >
+                {coordinator.name}
+              </a>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4">
           <a
-            href="https://www.instagram.com"
+            href="mailto:otriyo.ghosh.ece26@hitk.edu.in"
+            aria-label="Email"
             target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Instagram"
           >
             <svg
-              className="w-6 h-6 text-white hover:text-red-500 transition"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5A4.25 4.25 0 0 0 20.5 16.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5zM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm0 1.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7zm5.25-2a.75.75 0 1 1 0 1.5.75.75 0 0 1 0-1.5z" />
-            </svg>
-          </a>
-
-          <a href="mailto:resonance@hitk.edu.in" aria-label="Email">
-            <svg
-              className="w-6 h-6 text-white hover:text-red-500 transition"
+              className="w-7 h-7 text-white hover:text-yellow-500 transition"
               fill="currentColor"
               viewBox="0 0 24 24"
             >
               <path d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2Zm0 2v.01L12 13 4 6.01V6h16ZM4 18v-9.19l7.39 6.16a1 1 0 0 0 1.22 0L20 8.81V18H4Z" />
             </svg>
           </a>
-
           <a
-            href="https://www.facebook.com"
+            href="https://www.instagram.com/resonancehitk"
             target="_blank"
             rel="noopener noreferrer"
-            aria-label="Facebook"
+            aria-label="Instagram"
           >
             <svg
-              className="w-6 h-6 text-white hover:text-red-500 transition"
+              className="w-6 h-6 text-white hover:text-yellow-500 transition"
               fill="currentColor"
               viewBox="0 0 24 24"
             >
-              <path d="M13.5 22V12.5h3.25l.5-4h-3.75V6c0-1.1.3-1.85 1.9-1.85H17V1.2c-.9-.12-2.02-.2-3.1-.2-3.05 0-5.15 1.86-5.15 5.25v3h-3v4h3v9H13.5Z" />
+              <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5A4.25 4.25 0 0 0 20.5 16.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5zM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm0 1.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7zm5.25-2a.75.75 0 1 1 0 1.5.75.75 0 0 1 0-1.5z" />
             </svg>
           </a>
         </div>
-      </div>
+
+        <div className="text-sm font-montserrat ">&copy; Resonance 2025</div>
+      </footer>
     </div>
   );
 }
