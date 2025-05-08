0197dbfd00118c512f4d24d4e25ee70712018917
Ayush <ayushprokashyap@gmail.com>
2025-05-08 17:40:48 +0530

    card-data to events.json

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,4 +1,4 @@
-import { Lens } from "@/components/magicui/lens";
+import Lens from "@/components/magicui/lens";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -8,48 +8,16 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import events from "@/public/data/events.json";
 
 export default function Events() {
-  const cardData = [
-    {
-      img: "/images/scroll/img1.png",
-      title: "TedX HITK - 2025",
-      description: "Explore the minds shaping the future of technology.",
-      playlistLink: "https://youtube.com/playlist?list=innovation2025",
-    },
-    {
-      img: "/images/scroll/img2.png",
-      title: "Republic Day'25",
-      description: "Discover how compassion drives change worldwide.",
-      playlistLink: "https://youtube.com/playlist?list=humanity2025",
-    },
-    {
-      img: "/images/scroll/img3.png",
-      title: "Prothoma'24",
-      description: "Journey into a greener, cleaner tomorrow.",
-      playlistLink: "https://youtube.com/playlist?list=sustainability2025",
-    },
-    {
-      img: "/images/scroll/img4.png",
-      title: "Cadence'24",
-      description: "Dive into the art of storytelling and imagination.",
-      playlistLink: "https://youtube.com/playlist?list=creativity2025",
-    },
-    {
-      img: "/images/scroll/img5.png",
-      title: "Independence Day'24",
-      description: "Dive into the art of storytelling and imagination.",
-      playlistLink: "https://youtube.com/playlist?list=creativity2025",
-    },
-  ];
-
   return (
     <div className="min-h-screen w-full pt-32 px-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8 mb-10">
-        {cardData.map((card, i) => (
+        {events.map((event, i) => (
           <Card
             key={i}
-            className="relative max-w-md shadow-none border-none bg-gradient-to-r from-neutral-900 via-black to-neutral-900"
+            className="relative max-w-md shadow-none border-none bg-gradient-to-r bg-white/10 backdrop-blur-2xl"
           >
             <CardHeader>
               <Lens
@@ -58,27 +26,33 @@ export default function Events() {
                 isStatic={false}
                 ariaLabel={`Zoom Area ${i}`}
               >
-                <img src={card.img} alt={card.title} width={500} height={500} />
+                <img
+                  src={event.img}
+                  alt={event.title}
+                  width={500}
+                  height={500}
+                  className="w-full h-60 sm:h-64 md:h-72 lg:h-80 xl:h-96 object-cover rounded-lg"
+                />
               </Lens>
             </CardHeader>
             <CardContent>
-              <CardTitle className="text-2xl text-metal font-cinzel-decorative-bold">
-                {card.title}
+              <CardTitle className="text-xl md:text-2xl text-metal font-cinzel-decorative-bold">
+                {event.title}
               </CardTitle>
-              <CardDescription className="text-white">
-                {card.description}
+              <CardDescription className="text-white font-montserrat">
+                {event.description}
               </CardDescription>
             </CardContent>
-            <CardFooter className="space-x-4">
-              <Button>Let&apos;s go</Button>
+            <CardFooter className="space-x-4 font-montserrat">
+              <Button>Gallery</Button>
               <a
-                href={card.playlistLink}
+                href={event.playlistLink}
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <Button
                   variant="secondary"
-                  className="text-md text-white font-cinzel-decorative bg-gradient-to-r from-yellow-800 via-yellow-400 to-yellow-800"
+                  className="text-md cursor-pointer text-white font-cinzel-decorative bg-gradient-to-r from-yellow-800 via-yellow-400 to-yellow-800"
                 >
                   Playlist
                 </Button>
