0d12480aedc777350dd8d9142f8d35ae50a24646
Ayush <ayushprokashyap@gmail.com>
2025-05-08 13:57:29 +0530

    responsiveness-added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,9 +4,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
+import { useState } from "react";
+import { Menu } from "lucide-react";
 
 const Navbar = () => {
   const pathname = usePathname();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const routes = [
     { path: "/", label: "Home" },
@@ -15,38 +18,42 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="fixed top-6 left-1/2 transform -translate-x-1/2 shadow backdrop-blur-2xl h-20 w-[90%] md:w-[85%] bg-white/10 rounded-3xl px-12 py-3 flex justify-between items-center z-50">
-      <div className="flex items-center justify-center space-x-3 font-extrabold text-white text-2xl">
-        <div>
-          <Image
-            src="/images/resonance.png"
-            alt="resonance logo"
-            width={60}
-            height={60}
-          />
-        </div>
-        <div>
-          <span className="text-2xl font-cinzel-decorative-bold font-bold">
-            <span className="text-metal">Reso</span>
-            nance
-          </span>
-        </div>
+    <div className="fixed top-6 left-1/2 transform -translate-x-1/2 shadow backdrop-blur-2xl h-20 w-[90%] md:w-[85%] bg-white/10 rounded-3xl px-6 md:px-12 py-3 flex justify-between items-center z-50">
+      <div className="flex items-center space-x-3 font-extrabold text-white text-2xl">
+        <Image
+          src="/images/hitk-logo.png"
+          alt="resonance logo"
+          width={50}
+          height={50}
+        />
+        <span className="inline font-cinzel-decorative-bold text-2xl">
+          <span className="text-metal">Reso</span>
+          nance
+        </span>
       </div>
 
-      <div className="flex justify-center items-center space-x-4 text-md text-white font-cinzel-decorative tracking-widest">
-        <div className="flex space-x-6">
-          {routes.map(({ path, label }) => (
-            <Link
-              key={path}
-              href={path}
-              className={clsx("cursor-pointer  ", {
-                "text-yellow-400": pathname === path,
-              })}
-            >
-              {label}
-            </Link>
-          ))}
-        </div>
+      <div className="lg:hidden ">
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="text-white cursor-pointer"
+          aria-label="Toggle menu"
+        >
+          <Menu size={28} />
+        </button>
+      </div>
+
+      <div className="hidden lg:flex items-center space-x-6 text-white font-cinzel-decorative tracking-widest text-md">
+        {routes.map(({ path, label }) => (
+          <Link
+            key={path}
+            href={path}
+            className={clsx("cursor-pointer", {
+              "text-yellow-400": pathname === path,
+            })}
+          >
+            {label}
+          </Link>
+        ))}
         <Link
           href="/achievements"
           className={clsx(
@@ -59,6 +66,30 @@ const Navbar = () => {
           Achievements
         </Link>
       </div>
+
+      {isMenuOpen && (
+        <div className="absolute top-20 right-6 w-56 bg-[#3f3939]  rounded-xl shadow-md p-4 flex flex-col space-y-4 text-white z-50  font-cinzel-decorative text-md transition-all duration-700 ease-in-out lg:hidden ">
+          {routes.map(({ path, label }) => (
+            <Link
+              key={path}
+              href={path}
+              onClick={() => setIsMenuOpen(false)}
+              className={clsx("hover:text-yellow-500 cursor-pointer", {
+                "text-yellow-400": pathname === path,
+              })}
+            >
+              {label}
+            </Link>
+          ))}
+          <Link
+            href="/achievements"
+            onClick={() => setIsMenuOpen(false)}
+            className="cursor-pointer bg-gradient-to-r from-yellow-800 via-yellow-500 to-yellow-900 text-center py-1 px-3 rounded-lg"
+          >
+            Achievements
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
