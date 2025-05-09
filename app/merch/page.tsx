de61c761cac59c568ce4d3e327da02dc73ce907d
Ayush <ayushprokashyap@gmail.com>
2025-05-09 10:59:23 +0530

    Merch-page

---
+++ app/merch/page.tsx
@@ -0,0 +1,9 @@
+export default function Achievements() {
+  return (
+    <div className="justify-center h-screen w-full flex text-white items-center text-4xl md:text-6xl font-cinzel-decorative">
+      <div>
+        COMING <span className="text-metal">SOON</span>
+      </div>
+    </div>
+  );
+}
