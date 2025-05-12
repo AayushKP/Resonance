0433d69632be789b95c11862a40fec0cf5c6bc37
Ayush <ayushprokashyap@gmail.com>
2025-05-12 20:38:27 +0530

    metadata updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,26 @@ import { Analytics } from "@vercel/analytics/next";
 export const metadata: Metadata = {
   title: "Resonance - Official Music Club of HITK",
   description: "The Official Music Club of HITK",
+  openGraph: {
+    title: "Resonance - Music Club of HITK",
+    description: "Explore the sounds and spirit of HITK's official music club.",
+    url: "https://resonance-beta.vercel.app",
+    siteName: "Resonance",
+    images: [
+      {
+        url: "/resonance-img.png",
+        width: 1200,
+        height: 630,
+        alt: "Resonance: The Official Music Club of HITK",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
