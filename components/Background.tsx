3536fa2ee33cd2d1dc997ecce26f2958f3fb08f4
Ayush <ayushprokashyap@gmail.com>
2025-06-26 21:20:29 +0530

    reduced-music-notes

--- components/Background.tsx
+++ components/Background.tsx
@@ -113,7 +113,7 @@ export default function Background() {
       const isMobile = width < 768;
 
       const starCount = isMobile ? 1300 : 2300;
-      const noteCount = isMobile ? 20 : 50;
+      const noteCount = isMobile ? 20 : 35;
 
       createStars(starCount, width, height);
       createNotes(noteCount, width, height);
