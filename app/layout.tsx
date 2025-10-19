8b305ac25919c10b803d4f553812bc137d741794
Anirban Chandra <chandra723102@gmail.com>
2025-10-19 19:02:18 +0530

    log errors cleared adding app/types after running npm run build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-// @ts-expect-error Server Component
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Background from "@/components/Background";
